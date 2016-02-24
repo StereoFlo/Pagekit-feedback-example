b52b880241664cb0ee940588ff398dcf8816af7a
stereoflo <stereoflo@stereoflo.ru>
2016-02-24 16:53:12 +0300

    backend html fix

--- views/admin/index.php
+++ views/admin/index.php
@@ -4,14 +4,12 @@
 	<div class="uk-alert" v-if="!emails.length">{{ 'No entries' | trans }}</div>
 	<ul class="uk-list uk-list-line" v-if="emails.length">
 		<li class="uk-text-large" v-for="entry in emails">
-			<div class="uk-grid">
-			    <div class="uk-width-medium-1-1">
-			        <div class="uk-panel">
-						<div class="uk-panel-badge uk-badge">{{ entry.date }}</div>
-						<h3 class="uk-panel-title">{{ entry.name }} | {{ entry.email }}</h3>
-						<p>{{ entry.message }}</p>
-					</div>
-			    </div>
+			<div class="uk-width-medium-1-1">
+			    <div class="uk-panel uk-panel-box">
+					<div class="uk-panel-badge uk-badge">{{ entry.date }}</div>
+					<h3 class="uk-panel-title">{{ entry.name }} | {{ entry.email }}</h3>
+					{{ entry.message }}
+				</div>
 			</div>
         </li>
     </ul>
